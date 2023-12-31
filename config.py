32065f123f682918e0d0c9b39e199a47efbb0760
Linda Thomas-Fowler <linda@ac4lt.org>
2023-12-31 11:47:20 -0500

    initial commit

---
+++ config.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# conf.py - Device configuration file and shared logger construction
+# Part of the AlpycaDevice Alpaca skeleton/template device driver
+#
+# Author:   Robert B. Denny <rdenny@dc3.com> (rbd)
+#
+# Python Compatibility: Requires Python 3.7 or later
+# GitHub: https://github.com/ASCOMInitiative/AlpycaDevice
+#
+# -----------------------------------------------------------------------------
+# MIT License
+#
+# Copyright (c) 2022 Bob Denny
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# -----------------------------------------------------------------------------
+# Edit History:
+# 24-Dec-2022   rbd 0.1 Logging
+# 25-Dec-2022   rbd 0.1 More config items, separate logging section
+# 27-Dec-2022   rbd 0.1 Move shared logger construction and global
+#               var here. MIT license and module header. No mcast.
+#
+import sys
+import toml
+import logging
+
+#
+# This slimy hack is for Sphinx which, despite the toml.load() being
+# run only once on the first import, it can't deal with _dict not being
+# initialized or ?!?!?!?!? If you try to use getcwd() in the file name
+# here, it will also choke Sphinx. This cost me a day.
+#
+_dict = {}
+_dict = toml.load(f'{sys.path[0]}/config.toml')    # Errors here are fatal.
+_dict2 = {}
+try:
+    # ltf - this file, if it exists can override or supplement definitions in the normal config.tom.
+    # this facilitates putting the driver in a docker container where installation specific
+    # configuration can be put in a file that isn't pulled from a repository
+    _dict2 = toml.load('/alpyca/config.toml')
+except:
+    _dict2 = {}
+    # file is optional so it's ok if it isn't there
+
+def get_toml(sect: str, item: str):
+    setting = ''
+    s = None
+    try:
+        setting = _dict2[sect][item]
+    except:
+        try:
+            setting = _dict[sect][item]
+        except:
+            setting = ''
+
+    return setting
+
+class Config:
+    """Device configuration in ``config.toml``"""
+    # ---------------
+    # Network Section
+    # ---------------
+    ip_address: str = get_toml('network', 'ip_address')
+    port: int = get_toml('network', 'port')
+    # --------------
+    # Server Section
+    # --------------
+    location: str = get_toml('server', 'location')
+    verbose_driver_exceptions: bool = get_toml('server', 'verbose_driver_exceptions')
+    # --------------
+    # Device Section
+    # --------------
+    mqtt_server: str = get_toml('device', 'mqtt_server')
+    mqtt_port: int = get_toml('device', 'mqtt_port') 
+    mqtt_user: str = get_toml('device', 'mqtt_user')
+    mqtt_password: str = get_toml('device', 'mqtt_password')
+    topic_cloud_cover: str  = get_toml('device', 'topic_cloud_cover')
+    topic_probability_of_precipitation: str = get_toml('device', 'topic_probability_of_precipitation')
+    topic_event_rain: str = get_toml('device', 'topic_event_rain')
+    topic_forecast_precipitation_mm: str = get_toml('device', 'topic_forecast_precipitation_mm')
+    topic_rain_in_region: str = get_toml('device', 'topic_rain_in_region')
+    topic_wind_speed_avg: str = get_toml('device', "topic_wind_speed_avg")
+    limit_precipation_probability: float = get_toml('device', 'limit_precipation_probability')
+    # ---------------
+    # Logging Section
+    # ---------------
+    log_level: int = logging.getLevelName(get_toml('logging', 'log_level'))  # Not documented but works (!!!!)
+    log_to_stdout: str = get_toml('logging', 'log_to_stdout')
+    max_size_mb: int = get_toml('logging', 'max_size_mb')
+    num_keep_logs: int = get_toml('logging', 'num_keep_logs')
---
+++ mqttsafetymonitor.py
@@ -0,0 +1,135 @@
+# ASCOM Alpaca Observing Conditions low level device
+#
+# This class is meant to be used from the high level ObservingConditions class
+#
+# linda 2023-12-25 initial coding
+
+import paho.mqtt.client as mqtt
+import time
+from logging import Logger
+from threading import Lock
+from config import Config
+
+class MQTTSafetyMonitor:
+
+    # lock
+    _lock = None
+
+    # observing conditions values
+    _cloudCover = 0
+    _probabilityOfPrecipitation = 0
+    _forecastPrecipitation = 0
+    _rainInRegion = 0
+    _rainRate = 0
+    _limit_precipation_probability = 5
+    _windSpeedAvg = 0
+
+    # average period (seconds)
+    _averageperiod = 0
+
+    # to calculate rain rate
+    last_rain_reading = 0
+    current_rain_reading = 0
+    last_rain_time = 0
+    current_rain_time = 0
+
+
+    def __init__(self, logger):
+        # internal state
+        self.logger = logger
+        self._connected = False
+        self._lock = Lock()
+        # MQTT client
+        self.client = mqtt.Client("MQTTObservingConditions", userdata = self)
+        self.client.username_pw_set(Config.mqtt_user, Config.mqtt_password)
+        self.client.connect(Config.mqtt_server, Config.mqtt_port)
+        # safety limit for probability of precipitation
+        _limit_precipation_probability = Config.limit_precipation_probability
+        # add callbacks
+        self.client.message_callback_add(Config.topic_cloud_cover, on_message_cloud_cover)  
+        self.client.message_callback_add(Config.topic_probability_of_precipitation, on_message_probability_of_precipitation)  
+        self.client.message_callback_add(Config.topic_event_rain, on_message_event_rain)        
+        self.client.message_callback_add(Config.topic_forecast_precipitation_mm, on_message_forecast_precipitation_mm)     
+        self.client.message_callback_add(Config.topic_rain_in_region, on_message_rain_in_region)   
+        self.client.message_callback_add(Config.topic_wind_speed_avg, on_message_wind_speed_avg)
+        # subscriptions
+        self.client.subscribe(Config.topic_cloud_cover)
+        self.client.subscribe(Config.topic_probability_of_precipitation)     
+        self.client.subscribe(Config.topic_event_rain)   
+        self.client.subscribe(Config.topic_forecast_precipitation_mm)
+        self.client.subscribe(Config.topic_rain_in_region)
+        self.client.subscribe(Config.topic_wind_speed_avg)
+        # start handling subscriptions
+        self.client.loop_start()
+
+    @property
+    def connected(self) -> bool:
+        self._lock.acquire()
+        res = self._connected
+        self._lock.release()
+        return res
+    @connected.setter
+    def connected (self, connected: bool):
+        self._lock.acquire()
+        self._connected = connected
+        self._lock.release()
+        if connected:
+            self.logger.info('[connected]')
+        else:
+            self.logger.info('[disconnected]')    
+
+
+    @property
+    def issafe(self) -> bool:
+        self._lock.acquire()
+        res =  self._cloudCover != 100 and \
+            self._probabilityOfPrecipitation <= self._limit_precipation_probability and \
+            self._rainRate == 0 and \
+            self._forecastPrecipitation == 0 and \
+            not self._rainInRegion and self._windSpeedAvg < 10
+        self._lock.release()
+        return res     
+    
+# callbacks    
+def on_message_cloud_cover(client, userdata, msg):
+    userdata._lock.acquire()
+    userdata._cloudCover = float(msg.payload.decode('utf-8'))
+    userdata.logger.info(f"[cloud cover] value {userdata._cloudCover}")  
+    userdata._lock.release()
+
+def on_message_probability_of_precipitation(client, userdata, msg):
+    userdata._lock.acquire()
+    userdata._probabilityOfPrecipitation = float(msg.payload.decode('utf-8')) 
+    userdata.logger.info(f"[probability of precipitation] value {userdata._probabilityOfPrecipitation}")  
+    userdata._lock.release()
+
+def on_message_event_rain(client, userdata, msg):
+    userdata._lock.acquire()
+    userdata.last_rain_reading = userdata.current_rain_reading
+    userdata.current_rain_reading = float(msg.payload.decode('utf-8'))
+    userdata.last_rain_time = userdata.current_rain_time
+    userdata.current_rain_time = time.time()
+    rate = (userdata.current_rain_reading - userdata.last_rain_reading) / (userdata.current_rain_time - userdata.last_rain_time)
+    if rate < 0:
+        rate = 0
+    userdata._rainRate = rate * 3600  # convert mm/s to mm/hour
+    userdata.logger.info(f"[rain rate] value {userdata._rainRate}")  
+    userdata._lock.release()
+
+def on_message_forecast_precipitation_mm(client, userdata, msg):
+    userdata._lock.acquire()
+    userdata._forecastPrecipitation = float(msg.payload.decode('utf-8'))
+    userdata.logger.info(f"[forecast precipitation] value {userdata._forecastPrecipitation}")  
+    userdata._lock.release()
+
+def on_message_rain_in_region(client, userdata, msg):
+    userdata._lock.acquire()
+    userdata._rainInRegion = bool(msg.payload.decode('utf-8'))  
+    userdata.logger.info(f"[rain in region] value {userdata._rainInRegion}")  
+    userdata._lock.release()
+
+def on_message_wind_speed_avg(client, userdata, msg):
+    userdata._lock.acquire()
+    userdata._windSpeedAvg = float(msg.payload.decode('utf-8')) * (1000.0/3600.0)  # convert from km/h to m/s
+    userdata.logger.info(f"[wind speed] value {userdata._windSpeedAvg}")  
+    userdata._lock.release()    
